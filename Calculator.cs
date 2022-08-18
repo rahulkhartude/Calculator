93264f087865ccb08a0bc4461963beaa91383436
rahulkhartude <90925722+rahulkhartude@users.noreply.github.com>
2022-08-18 10:05:03 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyCalculator
+{
+    public class Calculator: INotifyPropertyChanged 
+    {
+      //  readonly int _result;
+        private int firstNumber;
+        public int FirstNumber
+        {
+            get { return firstNumber; }
+            set
+            {
+                firstNumber = value;
+                //if (PropertyChanged != null)
+                //{
+                //    PropertyChanged(this, new PropertyChangedEventArgs("FirstNumber"));
+                //}
+            }
+        }
+
+        private int secondNumber;
+        //public int SecondNumber { get; set; }
+        public int SecondNumber
+        {
+            get { return secondNumber; }
+            set
+            {
+                secondNumber = value;
+                
+            }
+        }
+
+        private int _result;
+
+        public int Result
+        {
+            get { return _result; }
+
+            set
+            {
+                _result = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Result"));
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Add(Object obj)
+        {
+             Result = FirstNumber + SecondNumber;
+        }
+        private void Substract(Object obj)
+        {
+            Result = FirstNumber - SecondNumber;
+        }
+
+        private void Multiplication(Object obj)
+        {
+            Result = FirstNumber * SecondNumber;
+        }
+
+        private void Division(Object obj)
+        {
+            Result = FirstNumber / SecondNumber;
+        }
+
+        public bool CanExecute(object parameter) //when to execute 
+        {
+            if (FirstNumber > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+          //  return true;
+        }
+
+        public bool CanExecuteMulDiv(object parameter) //when to execute 
+        {
+            if (SecondNumber > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //  return true;
+        }
+
+
+
+
+
+
+
+
+        private ICommand addCommand;
+         public ICommand AddCommand
+         {
+            get { return addCommand; }
+         }
+
+        private ICommand subCommand;
+        public ICommand SubCommand
+        {
+            get { return subCommand; }
+        }
+
+        private ICommand mulCommand;
+        public ICommand MulCommand
+        {
+            get { return mulCommand; }
+        }
+
+        private ICommand divCommand;
+        public ICommand DivCommand
+        {
+            get { return divCommand; }
+        }
+
+
+
+        public Calculator() 
+        {
+
+            addCommand = new RelayCommandCalc(Add, CanExecute);
+            subCommand = new RelayCommandCalc(Substract, CanExecute);
+            mulCommand = new RelayCommandCalc(Multiplication, CanExecuteMulDiv);
+            divCommand = new RelayCommandCalc(Division, CanExecuteMulDiv);
+        }
+    }
+}
---
+++ RelayCommandCalc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyCalculator
+{
+    public class RelayCommandCalc:ICommand
+    {
+
+        readonly Action<Object> _execute;
+        readonly Predicate<Object> _canExecute;
+
+        public RelayCommandCalc(Action<Object> execute) : this(execute, null)
+        { }
+
+        public RelayCommandCalc(Action<object> execute, Predicate<object> canExecute)
+        {//consructor
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute method missing");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+
+        }
+
+        public void Execute(object parameter) //what to execute
+        {
+            
+            _execute(parameter); //delegate invoke =method name
+            
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+     //   public event EventHandler CanExecuteChanged;
+    }
+}
